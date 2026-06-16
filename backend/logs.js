afad6af5cca3bb864080b385d6c8f7697110880f
ars4041 <ars4041@g.rit.edu>
2026-06-16 11:38:39 -0400

    logging system backend functionality implemented in logs.js.

--- backend/logs.js
+++ backend/logs.js
@@ -8,7 +8,6 @@
 const fs = require('node:fs/promises'); // Version: node@24.16.0
 const path = require('path'); // Version: node@24.16.0
 
-const code_log = new Map();
 const log_path = path.join(__dirname, 'user_logs');
 
 /**
@@ -47,27 +46,58 @@ async function removeUserLog(user) {
  * Stores the users code into their log directory.
  * 
  * @param user: The user to store the code for from their log directory.
- * @param key: The name to give the code file.
+ * @param title: The name to give the code file.
  */
-async function saveCode(user, key) {
+async function saveCode(user, title, code) {
+    await createUserLog(user); // safe guard if the user log doesn't already exist somehow
+    try {
+        const file_path = path.join(log_path, user, title);
+        await fs.writeFile(file_path, code, 'utf-8');
+        console.log(`Code saved for user: ${user} with title: ${title}`);
+    }
+    catch (err) {
+        console.error(`Code could not be saved for user: ${user} with title: ${title} -> Error: ${err.message}`);
+        return;
+    }
 }
 
 /**
  * Loads the users code from their log directory.
  * 
  * @param user: The user to load the code for from their log directory.
- * @param key: The name of the code file.
+ * @param title: The name of the code file.
  */
-async function loadCode(user, key) {
+async function loadCode(user, title) {
+    await createUserLog(user); // safe guard if the user log doesn't already exist somehow
+    let code;
+    try {
+        const file_path = path.join(log_path, user, title);
+        code = await fs.readFile(file_path, 'utf-8');
+        console.log(`Code loaded for user: ${user} with title: ${title}`);
+    }
+    catch (err) {
+        console.error(`Code could not be loaded for user: ${user} with title: ${title} -> Error: ${err.message}`);
+        code = null;
+    }
+    return code;
 }
 
 /**
  * Removes the users code from their log directory.
  * 
  * @param user: The user to remove the code for in their log directory.
- * @param key: The name of the code file.
+ * @param title: The name of the code file.
  */
-async function removeCode(user, key) {
+async function removeCode(user, title) {
+    try {
+        const file_path = path.join(log_path, user, title);
+        await fs.unlink(file_path);
+        console.log(`Code deleted for user: ${user} with title: ${title}`);
+    }
+    catch (err) {
+        console.error(`Code could not be deleted for user: ${user} with title: ${title} -> Error: ${err.message}`);
+        return;
+    }
 }
 
 /**
@@ -76,18 +106,25 @@ async function removeCode(user, key) {
  * @param user: The user to get the code file names for from their log directory.
  */
 async function getLogs(user) {
-}
-
-/**
- * Creates a unique file name for the users code file to be stored in their log directory.
- * 
- * @param user: The user to create the file name for.
- * @param key: The name to give the code file.
- */
-function createKey(user, key) {
+    await createUserLog(user); // safe guard if the user log doesn't already exist somehow
+    let logs;
+    try {
+        const user_path = path.join(log_path, user);
+        logs = await fs.readdir(user_path);
+        console.log(`Successfully retrieved logs for user: ${user}`);
+    }
+    catch (err) {
+        console.error(`Could not get logs for user: ${user} -> Error: ${err.message}`);
+        logs = [];
+    }
+    return logs;
 }
 
 module.exports = {
     createUserLog,
-    removeUserLog
+    removeUserLog,
+    saveCode,
+    loadCode,
+    removeCode,
+    getLogs
 };
