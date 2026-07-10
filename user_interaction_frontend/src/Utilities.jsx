d60aabf9c15ed51be30908441bbfd5c0e5d17789
ars4041 <ars4041@g.rit.edu>
2026-07-10 18:21:44 -0400

    Documentation for loadLogs function in Utilities.jsx in user_interaction_frontend added.

--- user_interaction_frontend/src/Utilities.jsx
+++ user_interaction_frontend/src/Utilities.jsx
@@ -1,10 +1,25 @@
-  // loads the current logs
-  export async function loadLogs(currentUser, forAdmin) {
+/**
+ * @file status_frontend/src/Utilities.jsx
+ * 
+ * @fileoverview Utility functions for user interaction frontend for RoboFleet (robotics-project.gccis.rit.edu)
+ * 
+ * @date 7/10/2026
+ * @author Aidan Sanderson
+ */
+
+/**
+ * Loads the logs for the given user. Uses the perm log fies for sending to the admin account so it shows the logs that have been deleted by the user.
+ * 
+ * @param {string} user: The user to load the logs for.
+ * @param {string} forAdmin: Is the log loading for the admin account?
+ * @returns: The log file paths for the given user (perm paths for giving to admin).
+ */
+  export async function loadLogs(user, forAdmin) {
     let logs = [];
     try {
       let response;
       if (forAdmin) {
-        response = await fetch(`/api/perm/${currentUser}`, {
+        response = await fetch(`/api/perm/${user}`, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
@@ -12,7 +27,7 @@
         });
     }
     else {
-        response = await fetch(`/api/log/${currentUser}`, {
+        response = await fetch(`/api/log/${user}`, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
@@ -29,7 +44,7 @@
       }
     }
     catch(err) {
-      console.error(`ERROR: cant load logs for User: ${currentUser}`);
+      console.error(`ERROR: cant load logs for User: ${user}`);
     }
     return logs;
   }
