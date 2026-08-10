9588d783b577c72ddb2be0541fcc9ec0f9a195da
ars4041 <ars4041@g.rit.edu>
2026-08-10 14:02:53 -0400

    Added a return home button to the login page.

--- user_interaction_frontend/src/pages/Login.jsx
+++ user_interaction_frontend/src/pages/Login.jsx
@@ -54,37 +54,54 @@ export default function Login({ onLoginSuccess }) {
     }
   };
 
+  const handleReturnHome = async (e) => {
+    e.preventDefault();
+    try {
+      window.location.href = 'https://robotics-project.gccis.rit.edu'
+    }
+    catch (err) {
+      console.error(`Error in trying to return to status page -> Error: ${err.message}`);
+    }
+  }
+
   return (
     <div className="login-container">
-      <h2 className="login-title">RoboFleet Login</h2>
+      <div className="login-box">
+        <h2 className="login-title">RoboFleet Login</h2>
       
-      <form onSubmit={handleLogin} className="login-form">
-        <div className="form-group">
-          <input 
-            type="text" 
-            value={username} 
-            onChange={(e) => setUsername(e.target.value)} 
-            placeholder="Username" 
-            className="form-input"
-            required 
-          />
-        </div>
-        <div className="form-group">
-          <input 
-            type="password" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-            placeholder="Password" 
-            className="form-input"
-            required 
-          />
-        </div>
-        <button type="submit" className="btn-submit">
-          Access Dashboard
-        </button>
-      </form>
+        <form onSubmit={handleLogin} className="login-form">
+          <div className="form-group">
+            <input 
+              type="text" 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)} 
+              placeholder="Username" 
+              className="form-input"
+              required 
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              placeholder="Password" 
+              className="form-input"
+              required 
+            />
+          </div>
+          <button type="submit" className="btn-submit">
+            Access Dashboard
+          </button>
+        </form>
+      </div>
 
-      {error && <p className="error-message">❌ {error}</p>}
+      <div className="below-login">
+        <button className="btn-home" onClick={ (e) => handleReturnHome(e)}>
+          Return Home
+        </button>
+        {error && <p className="error-message">❌ {error}</p>}
+      </div>
     </div>
   );
 }
